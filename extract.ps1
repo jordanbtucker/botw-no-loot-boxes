8b4e5e3a88665c9d556a3617dd4e7538b420ffde
Jordan Tucker <jordanbtucker@gmail.com>
2021-05-29 22:10:40 -0500

    feat: extract drop tables to src folder

--- extract.ps1
+++ extract.ps1
@@ -1,6 +1,10 @@
+if (-not (Test-Path src)) {
+  New-Item -ItemType Directory src
+}
+
 Get-ChildItem -File .\content\Actor\Pack\*.sbactorpack | ForEach-Object { sarc x $_.FullName }
 Get-ChildItem -File -Recurse -Include *.bdrop .\content\Actor\Pack\ | ForEach-Object {
   $file = Get-Item $_.FullName
-  $outPath = "$($file.Directory.FullName)\$($file.BaseName).yml"
+  $outPath = ".\src\$($file.BaseName).yml"
   aamp $file.FullName > $outPath
 }
