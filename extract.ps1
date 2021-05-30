ecf2bd8344ac10f7d9dc3e3ada1871c041ea74dc
Jordan Tucker <jordanbtucker@gmail.com>
2021-05-29 21:55:29 -0500

    fix: run aamp on content folder only

--- extract.ps1
+++ extract.ps1
@@ -1,5 +1,5 @@
 Get-ChildItem -File .\content\Actor\Pack\*.sbactorpack | ForEach-Object { sarc x $_.FullName }
-Get-ChildItem -File -Recurse -Include *.bdrop | ForEach-Object {
+Get-ChildItem -File -Recurse -Include *.bdrop .\content\Actor\Pack\ | ForEach-Object {
   $file = Get-Item $_.FullName
   $outPath = "$($file.Directory.FullName)\$($file.BaseName).yml"
   aamp $file.FullName > $outPath
