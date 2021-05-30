5915e2ace7383e9922e39e7b15f5e6ddef058a35
Jordan Tucker <jordanbtucker@gmail.com>
2021-05-29 21:53:41 -0500

    fix: don't delete non-bdrop files

--- extract.ps1
+++ extract.ps1
@@ -1,5 +1,4 @@
 Get-ChildItem -File .\content\Actor\Pack\*.sbactorpack | ForEach-Object { sarc x $_.FullName }
-Get-ChildItem -File -Recurse -Exclude *.bdrop,*.sbactorpack .\content\Actor\Pack\ | ForEach-Object { Remove-Item $_.FullName }
 Get-ChildItem -File -Recurse -Include *.bdrop | ForEach-Object {
   $file = Get-Item $_.FullName
   $outPath = "$($file.Directory.FullName)\$($file.BaseName).yml"
