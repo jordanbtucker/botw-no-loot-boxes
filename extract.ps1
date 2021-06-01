e3caf275478bd632e141458418877845003189c7
Jordan Tucker <jordanbtucker@gmail.com>
2021-05-31 22:40:46 -0500

    feat: reorganize file structure

--- extract.ps1
+++ extract.ps1
@@ -1,10 +1,30 @@
-if (-not (Test-Path src)) {
-  New-Item -ItemType Directory src
+# Extracts *.sbactorpack files and converts *.bdrop files to *.yml files.
+
+$packsDir = ".\src\packs"
+$tablesDir = ".\src\tables"
+$extractRoot = ".\tmp\content\Actor\Pack"
+
+# Create extract root directory
+if (-not (Test-Path $extractRoot)) {
+  New-Item -ItemType Directory -Force $extractRoot
 }
 
-Get-ChildItem -File .\content\Actor\Pack\*.sbactorpack | ForEach-Object { sarc x $_.FullName }
-Get-ChildItem -File -Recurse -Include *.bdrop .\content\Actor\Pack\ | ForEach-Object {
+# Extract the *.sbactorpack files
+Get-ChildItem -File "$packsDir\*.sbactorpack" | ForEach-Object {
+  $extractDir = "$extractRoot\$($_.BaseName)"
+  sarc x --directory $extractDir $_.FullName
+}
+
+# Create the tables directory
+if (-not (Test-Path $tablesDir)) {
+  New-Item -ItemType Directory -Force $tablesDir
+}
+
+# Convert each *.bdrop file to a *.yml file if it doesn't exist
+Get-ChildItem -File -Recurse -Include *.bdrop $extractRoot | ForEach-Object {
   $file = Get-Item $_.FullName
-  $outPath = ".\src\$($file.BaseName).yml"
-  aamp $file.FullName > $outPath
+  $outPath = "$tablesDir\$($file.BaseName).yml"
+  if (-not (Test-Path $outPath)) {
+    aamp $file.FullName > $outPath
+  }
 }
---
+++ update.ps1
@@ -0,0 +1,26 @@
+# Converts *.yml files to *.bdrop files and packs them into *.sbactorpack files.
+
+$outDir = ".\out\content\Actor\Pack"
+$packsDir = ".\src\packs"
+$tablesDir = ".\src\tables"
+$extractRoot = ".\tmp\content\Actor\Pack"
+
+# Convert each *.yml file to *.bdrop file
+Get-ChildItem -File "$tablesDir\*.yml" | ForEach-Object {
+  $file = Get-Item $_.FullName
+  $baseName = $file.BaseName
+  $outPath = "$extractRoot\$baseName\Actor\DropTable\$baseName.bdrop"
+  aamp "$($file.FullName)" "$outPath"
+}
+
+# Create out root directory
+if (-not (Test-Path $outDir)) {
+  New-Item -ItemType Directory -Force $outDir
+}
+
+# Copy *.sbactorpack files to out directory
+Get-ChildItem -File "$packsDir\*.sbactorpack" | ForEach-Object {
+  Copy-Item $_.FullName "$outDir\$($_.Name)"
+  $baseName = $_.BaseName
+  sarc u "$extractRoot\$baseName" "$outDir\$baseName.sbactorpack"
+}
